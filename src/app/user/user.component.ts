fd5b6c559ad2bebaff58c6bee3e89146624d7c21
Abdul Rahuman <abrahuman8870@gmail.com>
2024-06-04 23:42:57 -0400

    @Output

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,4 +1,4 @@
-import { Component, signal, computed ,Input} from '@angular/core';
+import { Component, signal, computed ,Input, input, Output, EventEmitter } from '@angular/core';
 import { DUMMY_USERS } from "../dummy-users";
 
 const randomIndex=Math.floor(Math.random()*DUMMY_USERS.length)
@@ -12,25 +12,39 @@ const randomIndex=Math.floor(Math.random()*DUMMY_USERS.length)
 })
 export class UserComponent {
 
-  @Input() avatar!:string;
-  @Input() name!:string;
+  // @Input() avatar!:string;
+  // @Input() name!:string;
+
+  // avatar=input<string>();
+
+  avatar=input.required<string>();
+  name=input.required<string>();
+  id=input<string>();
+  @Output() select=new EventEmitter();
 
   // If access specifier private is specified then it can accessed only within the class not outside
   // selectedUser=DUMMY_USERS[randomIndex]
 
-  selectedUser=signal(DUMMY_USERS[randomIndex])
+  // selectedUser=signal(DUMMY_USERS[randomIndex])
 
-  imagePath=computed(()=>
-    "assets/users/"+this.selectedUser().avatar);
+  // imagePath=computed(()=>
+  //   "assets/users/"+this.selectedUser().avatar);
 
   // get imagePath(){
-  //   return "assets/users/"+this.selectedUser().avatar;
+  //   return "assets/users/"+this.avatar();
   // }
+
+  imagePath=computed(()=>{
+    return "assets/users/"+this.avatar();
+  });
 
   onSelectedUser(){
     console.log("Clicked!!");
-    const randomIdx=Math.floor(Math.random()*DUMMY_USERS.length)
-    this.selectedUser.set(DUMMY_USERS[randomIdx])
+    // const randomIdx=Math.floor(Math.random()*DUMMY_USERS.length)
+    // this.selectedUser.set(DUMMY_USERS[randomIdx])
+    this.select.emit(this.id);
   }
+
+
 
 }
